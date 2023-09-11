99bd72967023d847eb5148d5e196e296df65d8cb
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-11 11:36:14 -0400

    Initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  fill("red")
+  noStroke()
 }
 
 function draw() {
   background(125);
+  fill("red")
+  rect(200, 200, 100, 100);
+  fill("green")
+  ellipse(30, 30, 30)
 }
