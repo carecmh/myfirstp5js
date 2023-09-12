f485b914cb1c0ac102490d36d22fb50e698eaf12
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-12 10:53:38 -0400

    Colors

--- sketch.js
+++ sketch.js
@@ -1,25 +1,26 @@
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
+  noStroke()
 
 }
 
 function draw() {
   background("white");
-  fill("Pink")
+  fill('#DF688A')
   rect(100, 100, 200, 200);
-fill("blue")
+fill('#039ED9')
 rect(150,150, 100, 100)
-fill("yellow")
+fill('#F3A9BB')
 rect(300, 300, 200, 200)
-fill("red")
+fill('#F0828B')
 rect(100, 300, 200, 200)
-fill("green")
+fill('#CB4C78')
 rect(300, 100, 200, 200)
-fill("purple")
+fill('#017BAD')
 rect(250, 150, 100, 100)
-fill("pink")
+fill('#3FB7D0')
 rect(150, 250, 100, 100)
-fill("orange")
+fill('#4CD0E7')
 rect(250, 250, 100, 100)
 }
