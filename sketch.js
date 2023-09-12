0bfcdca41312ee057b803568149213b8e376bb4b
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-12 10:46:21 -0400

    Layout (not right colors)

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
-  fill("red")
-  noStroke()
+
 }
 
 function draw() {
-  background(125);
-  fill("red")
-  rect(200, 200, 100, 100);
-  fill("green")
-  ellipse(30, 30, 30)
+  background("white");
+  fill("Pink")
+  rect(100, 100, 200, 200);
+fill("blue")
+rect(150,150, 100, 100)
+fill("yellow")
+rect(300, 300, 200, 200)
+fill("red")
+rect(100, 300, 200, 200)
+fill("green")
+rect(300, 100, 200, 200)
+fill("purple")
+rect(250, 150, 100, 100)
+fill("pink")
+rect(150, 250, 100, 100)
+fill("orange")
+rect(250, 250, 100, 100)
 }
